4492c30b6658739b6c6481edd64506818bc89a70
Ankit1598 <ankitchaudhari1598@gmail.com>
2021-03-11 12:32:50 +0530

    :tada: added template

---
+++ api/app.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/time')
+def get_current_time():
+    return {'time': datetime.now()}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [currentTime, setCurrentTime] = useState("");
+
+	useEffect(() => {
+		fetch("/time")
+			.then((res) => res.json())
+			.then((data) => {
+				setCurrentTime(String(data.time));
+			});
+	}, [currentTime]);
+	return (
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Learn React
+				</a>
+				<p>The current time is {currentTime}.</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
